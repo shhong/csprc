049edce6d0275d8b8437b4ad4475dff4fbd30ebc
Sungho Hong <shhong@oist.jp>
2012-07-04 14:02:48 +0200

    setup.m would not be necessary if the path is added properly.

--- setup.m
+++
@@ -1,6 +0,0 @@
-addpath /Users/shhong/Documents/MATLAB/csprc/functions
-addpath /Users/shhong/Documents/MATLAB/csprc/functions/utils
-addpath /Users/shhong/Documents/MATLAB/csprc/functions/utils/pursuits_homotopy
-addpath /Users/shhong/Documents/MATLAB/csprc/functions/utils/spikes
-addpath /Users/shhong/Documents/MATLAB/csprc/functions/model_selection
-
